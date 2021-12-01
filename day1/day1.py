000a1287830251c7633aa568050d316e40322582
ws1g17 <ws1g17@soton.ac.uk>
2021-12-01 18:46:21 +0000

    Updated answers for new input.txt

--- day1/day1.py
+++ day1/day1.py
@@ -3,10 +3,10 @@ with open("input") as file:
     lines = [int(line.rstrip()) for line in file.readlines()]
 
 # A
-# answer: 1548
+# answer: 1288
 print("A:", sum([x < y for x, y in zip(lines, lines[1:])]))
 
 # B
-# answer: 1589
+# answer: 1311
 sums = [sum(x) for x in zip(lines, lines[1:], lines[2:])]
 print("B:", sum([x < y for x, y in zip(sums, sums[1:])]))
