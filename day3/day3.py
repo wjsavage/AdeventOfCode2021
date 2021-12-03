b3ef575738bcf727c38dcd057ff4cbe470b485ec
ws1g17 <ws1g17@soton.ac.uk>
2021-12-03 11:27:40 +0000

    first clean up

--- day3/day3.py
+++ day3/day3.py
@@ -1,15 +1,18 @@
 from collections import Counter
+import re
 
 with open("input") as file:
     lines = [str(line.rstrip()) for line in file.readlines()]
 
-# print(len(lines[0]))
 cols = []
 for i in range(len(lines[0])):
     digits = []
     for line in lines:
         digits.append(line[i])
     cols.append(digits)
+
+# A
+# answer: 3912944
 
 bin = ""
 bin2 = ""
@@ -27,7 +30,7 @@ eps = int(bin2, 2)
 print("A:", gamma * eps)
 
 # B
-import re
+# answer: 4996233
 
 
 regex = ''
@@ -35,61 +38,38 @@ rows = lines.copy()
 i = 0
 
 while rows:
-    if i > len(lines[0]) - 1:
-        break
+    p = re.compile('^' + regex)
+    rows = list(filter(p.match, lines))
 
-    cols = [row[i] for row in rows]
+    cols = [row[len(regex)] for row in rows]
     c = Counter(cols)
-
-    if '0' not in c.keys():
-        regex += '1'
-        break
-    if '1' not in c.keys():
-        regex += '0'
+    if len(c.keys()) == 1:
         break
 
     if c['0'] > c['1']:
         regex += '0'
     else:
         regex += '1'
-    p = re.compile('^'+regex)
-    rows = [line for line in lines if p.match(line)]
 
-    i += 1
-
-p = re.compile('^'+regex)
-rows = [line for line in lines if p.match(line)]
 ox = rows[0]
 
 regex = ''
 rows = lines.copy()
-i = 0
 
 while rows:
-    if i > len(lines[0]) - 1:
+    p = re.compile('^' + regex)
+    rows = list(filter(p.match, lines))
+
+    cols = [row[len(regex)] for row in rows]
+    c = Counter(cols)
+    if len(c.keys()) == 1:
         break
 
-    cols = [row[i] for row in rows]
-    c = Counter(cols)
-    if '0' not in c.keys():
-        regex += '1'
-        break
-    if '1' not in c.keys():
-        regex += '0'
-        break
     if c['0'] <= c['1']:
-        regex += '0'
-    elif c['1'] == c['0']:
         regex += '0'
     else:
         regex += '1'
-    p = re.compile('^'+regex)
-    rows = [line for line in lines if p.match(line)]
 
-    i += 1
-
-p = re.compile('^'+regex)
-rows = [line for line in lines if p.match(line)]
 co = rows[0]
 
 print("B:", int(ox, 2) * int(co, 2))
