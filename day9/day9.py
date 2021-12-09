0624edce7576988b4b0c7c3021715e23131182b7
ws1g17 <ws1g17@soton.ac.uk>
2021-12-09 10:54:01 +0000

    Finished part B

--- day9/day9.py
+++ day9/day9.py
@@ -11,29 +11,29 @@ n_rows = len(rows)
 
 risk_levels = []
 
-for i in range(n_rows):
-    for j in range(n_cols):
-        n = rows[i][j]
-        neighbours = []
-        # get hoz neighbours
-        if i == 0:
-            neighbours.append(rows[i + 1][j])
-        elif i == n_rows - 1:
-            neighbours.append(rows[i - 1][j])
-        else:
-            neighbours += [rows[i-1][j], rows[i+1][j]]
-        # then get vert neighbours
-        if j == 0:
-            neighbours.append(rows[i][j+1])
-        elif j == n_cols - 1:
-            neighbours.append(rows[i][j-1])
-        else:
-            neighbours += [rows[i][j-1], rows[i][j + 1]]
 
-        if n < min(neighbours):
-            risk_levels.append(n + 1)
 
-print(sum(risk_levels))
+def get_neighbours(i, j):
+    neighbours = []
+    # get hoz neighbours
+    if i == 0:
+        neighbours.append((i + 1,j))
+    elif i == n_rows - 1:
+        neighbours.append((i - 1, j))
+    else:
+        neighbours.append((i + 1, j))
+        neighbours.append((i - 1, j))
+    # then get vert neighbours
+    if j == 0:
+        neighbours.append((i, j + 1))
+    elif j == n_cols - 1:
+        neighbours.append((i, j - 1))
+    else:
+        neighbours.append((i, j + 1))
+        neighbours.append((i, j - 1))
+
+    return neighbours
+
 
 # B thoughts
 # this is a tree search method,
@@ -47,3 +47,45 @@ print(sum(risk_levels))
     # for those that are not of value 9 and that are have not been visited
         # add their i, j to the queue
 # return basin accumulator
+
+def bfs_basin(i, j):
+    visited = set()
+    queue = []
+    count = 0
+    queue.append((i,j))
+    while queue:
+        p1, p2 = queue.pop()
+        if rows[p1][p2] == 9 or (p1, p2) in visited:
+            pass
+        else:
+            visited.add((p1,p2))
+            count += 1
+            for k, l in get_neighbours(p1, p2):
+                queue.append((k, l))
+
+    return count
+
+basins = []
+
+for i in range(n_rows):
+    for j in range(n_cols):
+        n = rows[i][j]
+        neighbours = get_neighbours(i, j)
+        neigh_val = [rows[i][j] for i, j in neighbours]
+
+        if n < min(neigh_val):
+            risk_levels.append(n + 1)
+            basins.append((i, j))
+
+# A: 550
+print("A:", sum(risk_levels))
+
+# B
+basin_sizes = []
+for i, j in basins:
+    basin_sizes.append(bfs_basin(i, j))
+
+from functools import reduce
+import heapq
+
+print("B:", reduce((lambda x, y: x * y), heapq.nlargest(3, basin_sizes)))
