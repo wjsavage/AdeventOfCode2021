3a21da01b2b2ad282c85da27a4ef0bc373ef1591
ws1g17 <ws1g17@soton.ac.uk>
2021-12-10 11:19:39 +0000

    Finall clean

--- day10/day10.py
+++ day10/day10.py
@@ -22,33 +22,30 @@ chunk_map = {
     '<': '>'
 }
 
-line_scores = []
-incompletes = []
+corrupt_line_scores, incomplete_lines = [], []
 for line in lines:
     open_queue = []
     corrupted = False
     for char in line:
         if char in chunk_map.keys():
             open_queue.append(char)
-        else:
-            if char != chunk_map[open_queue.pop()]:
-                corrupted = True
-                line_scores.append(point_map_a[char])
+        elif char != chunk_map[open_queue.pop()]:
+            corrupted = True
+            corrupt_line_scores.append(point_map_a[char])
+            break
 
-        if corrupted: break
     if not corrupted:
-        incompletes.append(open_queue)
+        incomplete_lines.append(open_queue)
 
 # A: 166191
-print("A:", sum(line_scores))
+print("A:", sum(corrupt_line_scores))
 
 from functools import reduce
 incomplete_scores = []
-for stack in incompletes:
+for stack in incomplete_lines:
     scores = [0] + [point_map_b[r] for r in stack[::-1]]
     incomplete_scores.append(reduce((lambda x, y: 5 * x + y), scores))
 
 incomplete_scores.sort()
-i = int((len(incomplete_scores) + 1) / 2) - 1
 # B: 1152088313
-print("B:", incomplete_scores[i])
+print("B:", incomplete_scores[int((len(incomplete_scores) + 1) / 2) - 1])
