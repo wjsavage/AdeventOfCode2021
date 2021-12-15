a280fef0ace41d8d5d7d27de74fe351c15a6333e
ws1g17 <ws1g17@soton.ac.uk>
2021-12-15 13:29:19 +0000

    Used heap rather than queue to dramatically reduce runtime

--- day15/day15.py
+++ day15/day15.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from itertools import repeat
+import heapq
 
 with open("input") as file:
     rows = [list(map(int, list(line.rstrip()))) for line in file.readlines()]
@@ -9,17 +10,18 @@ def is_valid_pos(pos, m):
     return pos[0] in range(len(m)) and pos[1] in range(len(m[0]))
 
 
-def search_grid(a, b, grid):
+def search_grid(a, grid):
 
     queue = []
+    heapq.heapify(queue)
+    heapq.heappush(queue, a)
+    end = (len(grid[0]) - 1, len(grid) - 1)
     point_costs = defaultdict(int)
 
-    queue.append(a)
-
     while queue:
-        cost, x, y = queue.pop(0)
+        cost, x, y = heapq.heappop(queue)
 
-        if x == b[0] and y == b[1]:
+        if x == end[0] and y == end[1]:
             return cost
 
         for x1, y1 in [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]:
@@ -28,15 +30,8 @@ def search_grid(a, b, grid):
                 if (x1, y1) in point_costs.keys() and point_costs[(x1, y1)] <= new_cost:
                     continue
                 point_costs[(x1, y1)] = new_cost
-                queue.append((new_cost, x1, y1))
-        queue = sorted(queue)
-        print(queue)
-# 423
+                heapq.heappush(queue, (new_cost, x1, y1))
 
-
-# start = (0, 0, 0)
-# end = (len(rows[0]) - 1, len(rows) - 1)
-# print(search_grid(start, end, rows))
 
 
 def inc(x, i):
@@ -44,21 +39,24 @@ def inc(x, i):
     return y if y < 10 else y % 9
 
 
-big_grid = []
-n = 5
-for j in range(n):
-    for row in rows:
-        long_row = []
-        for i, x in enumerate(repeat(row, n)):
-            long_row = long_row + [inc(r, i + j) for r in x]
-        big_grid.append(long_row)
+def gen_grid_multiple(n, grid):
+    large_grid = []
+    for j in range(n):
+        for row in grid:
+            long_row = []
+            for i, x in enumerate(repeat(row, n)):
+                long_row = long_row + [inc(r, i + j) for r in x]
+            large_grid.append(long_row)
+    return large_grid
+
 
 start = (0, 0, 0)
-end = (len(big_grid[0]) - 1, len(big_grid) - 1)
-print(end)
-print(big_grid[49][49])
-# print(*big_grid, sep='\n')
-print(search_grid(start, end, big_grid))
+# A: 423
+print("A:", search_grid(start, rows))
+
+big_grid = gen_grid_multiple(5, rows)
+# B: 2778
+print("B:", search_grid(start, big_grid))
 
 
 
